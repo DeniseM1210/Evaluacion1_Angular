f801eedfea3ef9aeedb9468e4277d414f2527b08
Denise <denisemorales8764@gmail.com>
2025-02-21 21:51:56 -0600

    Se crearon las interfaces de login y registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+
+const appRoutes = [
+    {path: "", component: AppComponent, pathMatch: "full"},
+    {path: "login", component: LoginComponent, pathMatch: "full"},
+    {path: "register", component: RegistroComponent, pathMatch: "full"},
+];
 
 export const routes: Routes = [];
